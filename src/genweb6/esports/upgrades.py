d97fc4a79523a50f50e97a9ab7862f37cbe50d7f
Ruben Padilla Mateu <ruben.padilla.mateu@pc68649.estacions.upcnet.es>
2023-05-23 13:47:15 +0200

    [ADD] Functionality of upgrading package

---
+++ src/genweb6/esports/upgrades.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from Products.CMFPlone.utils import get_installer
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def upgrade_by_reinstall(context):
+    qi = get_installer(context)
+    qi.uninstall_product("genweb6.esports")
+    qi.install_product("genweb6.esports")
+    qi.uninstall_product("plone.app.mosaic")
+    qi.install_product("plone.app.mosaic")
