af721175f1545f1e1eb22d689ddf32e6a55e647b
Ruben Padilla Mateu <ruben.padilla.mateu@pc68649.estacions.upcnet.es>
2023-08-07 11:58:06 +0200

    [UPD] Collection view

--- src/genweb6/esports/browser/views/collection.py
+++ src/genweb6/esports/browser/views/collection.py
@@ -1,8 +1,10 @@
 from Products.Five.browser import BrowserView
 from zope.component.hooks import getSite
+from plone.app.contenttypes.browser.collection import CollectionView
 
 
-class CollectionView(BrowserView):
+
+class EsportsCollectionView(CollectionView):
 
     def __init__(self, context, request):
         self.context = context
@@ -13,7 +15,8 @@ class CollectionView(BrowserView):
 
     def get_info(self):
         """ Get needed info in the template """
-        #import ipdb; ipdb.set_trace()
+
+        
         leadimage = '' if not self.context.picture else self.context.absolute_url() + '/@@images/picture'
         text = '' if not self.context.text else self.context.text.output
         info = {
@@ -22,3 +25,34 @@ class CollectionView(BrowserView):
             'text': text,
         }
         return info
+    
+    def get_items(self):
+        results = self.results()
+        items = []
+        remote_url = 'https://esportsonline.upc.edu/'
+        for res in results:
+            obj = res.getObject()
+            img = ''
+
+            if hasattr(obj, 'image'):
+                img = obj.absolute_url() + '/@@images/image'
+            elif hasattr(obj, 'img'):
+                img = remote_url + getattr(obj, 'img')
+            
+            items.append({
+                'title': obj.Title(),
+                'image': img,
+                'url': obj.absolute_url(),
+                'omesa_link': '' if not hasattr(obj, 'enlace') else obj.enlace,
+                'dates': '' if not hasattr(obj, 'texper') else obj.texper
+            })
+
+        return items
+    
+    def results(self, **kwargs):
+        kwargs.setdefault('batch', True)
+        kwargs.setdefault('b_size', self.b_size)
+        kwargs.setdefault('b_start', self.b_start)
+        results = self.collection_behavior.results(**kwargs)
+        """ for item in batch: item.getObject() """
+        return results
