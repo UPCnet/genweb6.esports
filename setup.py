c9a27ecbc5254429485a62c97de00b12b6c8e054
Ruben Padilla Mateu <ruben.padilla.mateu@ithinkupc.com>
2023-10-26 08:14:48 +0200

    Back to development: 1.8

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='genweb6.esports',
-    version='1.7',
+    version='1.8.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
